bd3a13052390b2e569ca79e969e9e2c6f41db091
Erik Roelofs <eroelofs@zigwebsoftware.nl>
2016-05-04 08:31:31 +0200

    Drawing a disc the naive way

--- main.lua
+++ main.lua
@@ -10,9 +10,15 @@ function love.draw()
   drawTower(1)
   drawTower(2)
   drawTower(3)
+  
+  drawDisc(8)
 end
 
 function drawTower(towerNum)
     love.graphics.rectangle("fill", 100 + (towerNum-1)*200,500,150,20)
-    love.graphics.rectangle("fill", 170+ (towerNum-1)*200,100,10,400)
+    love.graphics.rectangle("fill", 170 + (towerNum-1)*200,100,10,400)
+end
+
+function drawDisc(size)
+  love.graphics.rectangle("fill", 175 - ((size * 10 + 40) / 2), 465, size * 10 + 40, size*3 + 10)
 end
