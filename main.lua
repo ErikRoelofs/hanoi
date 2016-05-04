de613db0e834c44fe4030d6b5229496155bc40b1
Erik Roelofs <eroelofs@zigwebsoftware.nl>
2016-05-04 08:27:42 +0200

    Drawing towers with simple math function

--- main.lua
+++ main.lua
@@ -13,18 +13,6 @@ function love.draw()
 end
 
 function drawTower(towerNum)
-  if towerNum == 1 then
-    love.graphics.rectangle("fill", 100,500,150,20)
-    love.graphics.rectangle("fill", 170,100,10,400)
-  end
-  
-  if towerNum == 2 then
-    love.graphics.rectangle("fill", 300,500,150,20)
-    love.graphics.rectangle("fill", 370,100,10,400)
-  end
-  
-  if towerNum == 3 then
-    love.graphics.rectangle("fill", 500,500,150,20)
-    love.graphics.rectangle("fill", 570,100,10,400)
-  end
+    love.graphics.rectangle("fill", 100 + (towerNum-1)*200,500,150,20)
+    love.graphics.rectangle("fill", 170+ (towerNum-1)*200,100,10,400)
 end
