f8e924e76d1997fa613d1361df2415d645b5650d
Erik Roelofs <eroelofs@zigwebsoftware.nl>
2016-05-04 08:26:56 +0200

    Drawing towers with a simple function

--- main.lua
+++ main.lua
@@ -7,13 +7,24 @@ function love.update(dt)
 end
 
 function love.draw()
-  love.graphics.rectangle("fill", 100,500,150,20)
-  love.graphics.rectangle("fill", 170,100,10,400)
+  drawTower(1)
+  drawTower(2)
+  drawTower(3)
+end
+
+function drawTower(towerNum)
+  if towerNum == 1 then
+    love.graphics.rectangle("fill", 100,500,150,20)
+    love.graphics.rectangle("fill", 170,100,10,400)
+  end
   
-  love.graphics.rectangle("fill", 300,500,150,20)
-  love.graphics.rectangle("fill", 370,100,10,400)
-
-  love.graphics.rectangle("fill", 500,500,150,20)
-  love.graphics.rectangle("fill", 570,100,10,400)
-
+  if towerNum == 2 then
+    love.graphics.rectangle("fill", 300,500,150,20)
+    love.graphics.rectangle("fill", 370,100,10,400)
+  end
+  
+  if towerNum == 3 then
+    love.graphics.rectangle("fill", 500,500,150,20)
+    love.graphics.rectangle("fill", 570,100,10,400)
+  end
 end
