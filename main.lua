017bbb46a835b42661c83a720753e6856caf60d1
Erik Roelofs <eroelofs@zigwebsoftware.nl>
2016-05-04 08:25:42 +0200

    Drawing simple towers with rectangles

--- main.lua
+++ main.lua
@@ -7,5 +7,13 @@ function love.update(dt)
 end
 
 function love.draw()
+  love.graphics.rectangle("fill", 100,500,150,20)
+  love.graphics.rectangle("fill", 170,100,10,400)
   
+  love.graphics.rectangle("fill", 300,500,150,20)
+  love.graphics.rectangle("fill", 370,100,10,400)
+
+  love.graphics.rectangle("fill", 500,500,150,20)
+  love.graphics.rectangle("fill", 570,100,10,400)
+
 end
